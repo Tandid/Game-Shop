4d7170510da782766fb302227cfd0121c5508a42
Tandid Alam <tandid.alam@gmail.com>
2020-05-06 23:29:55 -0400

    Added validation and admin to users model

--- server/db/models/index.js
+++ server/db/models/index.js
@@ -4,13 +4,6 @@ const Category = require('./category')
 const OrderItems = require('./order-items')
 const Order = require('./order')
 
-/**
- * If we had any associations to make, this would be a great place to put them!
- * ex. if we had another model called BlogPost, we might say:
- *
- *    BlogPost.belongsTo(User)
- */
-
 Category.hasMany(Product)
 Product.belongsTo(Category)
 
