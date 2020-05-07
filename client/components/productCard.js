f40d6263dfe4bee1c0c55e9784815865a9811f7b
Tandid Alam <tandid.alam@gmail.com>
2020-05-07 01:46:25 -0400

    changed component to class

--- client/components/productCard.js
+++ client/components/productCard.js
@@ -2,28 +2,23 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 
-const ProductCard = ({
-  id,
-  title,
-  imageURL,
-  price,
-  inventory,
-  products,
-  cart
-}) => {
-  return (
-    <li key={id} className="card">
-      <h4>{title}</h4>
-      <br />
-      <img src={imageURL} />
-      <p>${price}</p>
-      <Link to={`/products/${id}`} className="productLink">
-        More Details
-      </Link>
-      <button onClick={ev => cart.items.push(`${id}`)}> Add to Cart </button>
-      <p>Quantity: {inventory}</p>
-    </li>
-  )
+class ProductCard extends React.Component {
+  render() {
+    const {id, title, imageURL, inventory, price} = this.props
+    return (
+      <li key={id} className="card">
+        <h4>{title}</h4>
+        <br />
+        <img src={imageURL} />
+        <p>${price}</p>
+        <Link to={`/products/${id}`} className="productLink">
+          More Details
+        </Link>
+        <button onClick={ev => cart.items.push(`${id}`)}>Add to Cart</button>
+        <p>Quantity: {inventory}</p>
+      </li>
+    )
+  }
 }
 
 const mapStateToProps = ({products, cart}) => {
