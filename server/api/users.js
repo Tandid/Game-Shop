a8d9e37ba49d7aef58284d9d676d348c467da057
Tandid Alam <tandid.alam@gmail.com>
2020-05-19 17:22:10 -0400

    push to heroku

--- server/api/users.js
+++ server/api/users.js
@@ -1,8 +1,7 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
-const {isAdmin} = require('./security')
 
-router.get('/', isAdmin, async (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
     const users = await User.findAll({
       // explicitly select only the id and email fields - even though
